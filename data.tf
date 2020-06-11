5d58192a6d7c27bdae89a7bf290281cc6e9b7b25
Ramcoumar MURALI <ramcoumar.murali@gmail.com>
2020-06-12 00:29:36 +0200

    fixed typo

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "template_file" "ecs_scheduled_service" {
   template = var.container_definitions
-  vars {
-    command                  = var.command
+  vars = {
+    command = var.command
   }
 }
